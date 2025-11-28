7d3c0cf2bd6fd63dd83d607ee31f7544396defe1
Stepan939 <147822713+Stepan939@users.noreply.github.com>
2025-11-28 14:59:06 +0500

    Update rms.cpp

--- 01_week/tasks/rms/rms.cpp
+++ 01_week/tasks/rms/rms.cpp
@@ -1,7 +1,20 @@
-#include <cstdef>
-#include <stdexcept>
+#include <cmath>
 
+double CalculateRMS(const double* data, int size) {
 
-double CalculateRMS(double values[], size_t size) {
-    throw std::runtime_error{"Not implemented"};
+    if (size == 0) {
+        return 0.0;
+    }
+    
+
+    double sum_of_squares = 0.0;
+    for (int i = 0; i < size; ++i) {
+        sum_of_squares += data[i] * data[i];
+    }
+    
+  
+    double mean_square = sum_of_squares / size;
+    double rms = std::sqrt(mean_square);
+    
+    return rms;
 }
