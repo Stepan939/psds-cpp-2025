966d552d8cf87d83a63abdc4093de10f2ab63e36
Stepan939 <147822713+Stepan939@users.noreply.github.com>
2025-11-28 14:42:42 +0500

    Update char_changer.cpp

--- 01_week/tasks/char_changer/char_changer.cpp
+++ 01_week/tasks/char_changer/char_changer.cpp
@@ -1,7 +1,80 @@
-#include <cstddef>
-#include <stdexcept>
+#include <cctype>
+#include <cstdint>
 
-
-size_t CharChanger(char array[], size_t size, char delimiter = ' ') {
-    throw std::runtime_error{"Not implemented"};
+char* CharChanger(char* str, char delimeter) {
+    char* read = str;   
+    char* write = str;  
+    char current_char;
+    int count;
+    
+    while (*read != '\0') {
+        current_char = *read;
+        count = 1;
+        
+       
+        while (*(read + 1) == current_char) {
+            count++;
+            read++;
+        }
+        
+       
+        if (current_char == ' ') {
+           
+            *write++ = delimeter;
+        }
+        else if (std::isdigit(current_char)) {
+           
+            if (count >= 10) {
+                *write++ = '*';
+                *write++ = '0';
+            } else {
+                *write++ = '*';
+                if (count > 1) {
+                    *write++ = '0' + count;
+                }
+            }
+        }
+        else if (std::isupper(current_char)) {
+            
+            if (count >= 10) {
+                *write++ = current_char;
+                *write++ = '0';
+            } else {
+                *write++ = current_char;
+                if (count > 1) {
+                    *write++ = '0' + count;
+                }
+            }
+        }
+        else if (std::islower(current_char)) {
+           
+            char upper_char = std::toupper(current_char);
+            if (count >= 10) {
+                *write++ = upper_char;
+                *write++ = '0';
+            } else {
+                *write++ = upper_char;
+                if (count > 1) {
+                    *write++ = '0' + count;
+                }
+            }
+        }
+        else {
+            
+            if (count >= 10) {
+                *write++ = '_';
+                *write++ = '0';
+            } else {
+                *write++ = '_';
+                if (count > 1) {
+                    *write++ = '0' + count;
+                }
+            }
+        }
+        
+        read++; 
+    }
+    
+    *write = '\0'; // Завершаем строку
+    return write;  // Возвращаем указатель на конец
 }
