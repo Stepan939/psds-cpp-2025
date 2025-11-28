7865e0168d1a138019126fbe06702c35445bf10f
Stepan939 <147822713+Stepan939@users.noreply.github.com>
2025-11-28 14:28:09 +0500

    Update rms.cpp

--- 01_week/tasks/rms/rms.cpp
+++ 01_week/tasks/rms/rms.cpp
@@ -1,18 +1,17 @@
 #include <cmath>
 
 double CalculateRMS(const double* data, int size) {
-    // Обработка пустого массива
+
     if (size == 0) {
         return 0.0;
     }
     
-    // Вычисление суммы квадратов элементов
+
     double sum_of_squares = 0.0;
     for (int i = 0; i < size; ++i) {
         sum_of_squares += data[i] * data[i];
     }
-    
-    // Вычисление среднеквадратического значения
+ 
     double mean_square = sum_of_squares / size;
     double rms = std::sqrt(mean_square);
     
